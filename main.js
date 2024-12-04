773c1cf539179efcd33ee82fa198477aee48daba
IRALAZARCHUK <ira.lazarchuk12@gmail.com>
2024-12-05 00:24:11 +0200

    init

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-console.log('main js');
+const fs = require('node:fs/promises');
+const path = require('node:path');
+
+
+const foo = async () => {
+    const baseFolder = 'baseFolder';
+    await fs.mkdir(baseFolder, { recursive: true });
+    for (let i = 1; i <= 5; i++) {
+        const newFolder = path.join(baseFolder, `newFolder${i}`);
+        await fs.mkdir(newFolder);
+
+        for (let j = 1; j <= 5; j++) {
+            const test = path.join(newFolder, `test${j}.txt`);
+            await fs.writeFile(test, `Hello World`);
+        }
+    }
+}
+
+void foo();
